234e90842c7dd4108fc2ecb11848c8bf6db81c20
Qluxzz <andre.eric.andersson@gmail.com>
2026-03-30 18:02:37 +0200

    Make sure it's possible to represent nullable values

--- Shared.Host.Test/ResponseModelTest.cs
+++ Shared.Host.Test/ResponseModelTest.cs
@@ -3,12 +3,63 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi;
 using Microsoft.OpenApi.Reader;
 
 namespace Shared.Host.Test;
 
 public class ResponseModelTest
 {
+    [Fact]
+    public async Task NullableEnumsShouldBeDocumentedInSwaggerAsOptional()
+    {
+        var host = new SharedHostBuilder()
+            .WithSwagger()
+            .Build(services =>
+            {
+                // This is what .UseTestServer does
+                services.AddSingleton<IHostLifetime, NoopHostLifetime>();
+                services.AddSingleton<IServer, TestServer>();
+
+                services.AddEndpointsApiExplorer();
+            });
+
+        host.MapGet("/test", () => TypedResults.Ok(new TestWithNullableEnum(TestEnum.Baz)));
+
+        await host.StartAsync(CancellationToken.None);
+
+        var response = await host.GetTestClient().GetByteArrayAsync("/swagger/v1/swagger.json");
+
+        await File.WriteAllBytesAsync("output", response);
+
+        using var stream = new MemoryStream(response);
+
+        var reader = new OpenApiJsonReader();
+
+        var (document, diagnostic) = reader.Read(stream, new Uri("http://localhost"), new());
+
+        Assert.NotNull(diagnostic);
+        Assert.Empty(diagnostic.Warnings);
+
+        Assert.NotNull(document?.Components?.Schemas);
+        var enumSchema = Assert.Contains(nameof(TestEnum), document.Components.Schemas);
+        Assert.NotNull(enumSchema);
+
+        var recordSchema = Assert.Contains(
+            nameof(TestWithNullableEnum),
+            document.Components.Schemas
+        );
+        Assert.NotNull(recordSchema.Properties);
+
+        var recordSchemaEnumSchema = Assert.IsType<OpenApiSchemaReference>(
+            recordSchema.Properties["nullable"]
+        );
+
+        Assert.Equal(recordSchemaEnumSchema.Target, enumSchema);
+    }
+
+    record TestWithNullableEnum(TestEnum? Nullable);
+
     [Fact]
     public async Task EnumsShouldBeReturnedAsStrings()
     {
@@ -66,7 +117,7 @@ public class ResponseModelTest
         Assert.NotNull(document.Components.Schemas);
         Assert.NotEmpty(document.Components.Schemas);
         Assert.NotEmpty(document.Paths);
-        var schema = Assert.Contains("TestEnum", document.Components.Schemas);
+        var schema = Assert.Contains(nameof(TestEnum), document.Components.Schemas);
         Assert.NotNull(schema.Enum);
         Assert.Equivalent(
             new List<string>() { "foo", "bar", "baz" },
